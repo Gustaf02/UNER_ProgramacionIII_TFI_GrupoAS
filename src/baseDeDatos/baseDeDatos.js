3211a4f40e209d6ab2f5c5ee909d3e569f434689
xtsulyts <531xtsulyts@gmail.com>
2025-09-07 21:41:50 -0300

    conexion con mysql server en ubuntu

--- src/baseDeDatos/baseDeDatos.js
+++ src/baseDeDatos/baseDeDatos.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
