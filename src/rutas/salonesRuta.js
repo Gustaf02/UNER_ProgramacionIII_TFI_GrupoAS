2c03e5f75c621e8f50a37d10767c758e796ae11a
xtsulyts <531xtsulyts@gmail.com>
2025-09-09 19:55:42 -0300

    servidor frontend corriendo ok.

--- src/rutas/salonesRuta.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const salonesController = require('../controladores/salonesControlador');
-
-// Rutas públicas (acceso sin autenticación)
-router.get('/', salonesController.getAll);
-router.get('/:id', salonesController.getById);
-
-module.exports = router;
