7bab49cf2e98bb610c8f3ce37eeb67fdfb57a862
xtsulyts <531xtsulyts@gmail.com>
2025-10-10 16:23:27 -0300

    ruta /api/v1/autenticacion/registro ok, crea usuario en base de datos.

--- src/app.js
+++ src/app.js
@@ -25,3 +25,17 @@ app.get('/estado', (req, res) => {
 });
 
 app.use(manejadorErrores);
+
+/**
+ * UPDATE tpintegrador_db.usuarios 
+SET password = MD5('nuevaContraseña') 
+WHERE email = 'oscram@correo.com';
+
+con este comando se actualiza la contrasenia para hacer login
+
+tipo_usuario 1 = oscram@correo.com
+tipo_usuario 2 = wilcor@correo.com
+tipo_usuario 3 = alblop@correo.com
+
+
+ */
