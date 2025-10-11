52a90b3a0497cc42c38ed1b90badf505974fd29a
xtsulyts <531xtsulyts@gmail.com>
2025-10-11 15:46:23 -0300

    creo front para puntos extra TFI, si tienen node 19 o anterior correr desde la raiz el .nvmrc con nvm use para activar un entorno con node 20.19.0

--- src/bd/conexion.js
+++ src/bd/conexion.js
@@ -7,4 +7,5 @@ export const conexion = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
 });
