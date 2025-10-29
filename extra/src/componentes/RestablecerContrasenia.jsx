5a0904ac5e39f0cb7e9e3423e012e7f591580ad9
xtsulyts <531xtsulyts@gmail.com>
2025-10-29 00:14:23 -0300

    componentes RestablecerContrasenia y pages RestablecerContraseniaPages

---
+++ extra/src/componentes/RestablecerContrasenia.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RestablecerContrasenia = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus pariatur consequatur aspernatur dolorem amet natus nihil, rem, quod voluptates neque aliquid eaque saepe labore quae laboriosam ratione vel sit?
+      
+    </div>
+  )
+}
+
+export default RestablecerContrasenia
